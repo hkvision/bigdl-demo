0d4202857d01810acbca75fea23d9c0df9db60d9
Kai Huang <huangkaivision@gmail.com>
2022-07-29 15:29:06 +0800

    Update and rename merged_submit_yarn_cluster.sh to submit_yarn_cluster.sh

--- yarn/submit_yarn_cluster.sh
+++ yarn/submit_yarn_cluster.sh
@@ -15,9 +15,7 @@ ${SPARK_HOME}/bin/spark-submit \
     --py-files ${BIGDL_HOME}/python/bigdl-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip \
     --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=environment/bin/python \
     --conf spark.executorEnv.PYSPARK_PYTHON=environment/bin/python \
-    --conf spark.yarn.appMasterEnv.http_proxy=http://child-prc.intel.com:913 \
-    --conf spark.yarn.appMasterEnv.https_proxy=http://child-prc.intel.com:913 \
     --conf spark.yarn.appMasterEnv.ARROW_LIBHDFS_DIR=/opt/cloudera/parcels/CDH-5.15.2-1.cdh5.15.2.p0.3/lib64 \
     --conf spark.executorEnv.ARROW_LIBHDFS_DIR=/opt/cloudera/parcels/CDH-5.15.2-1.cdh5.15.2.p0.3/lib64 \
-    --jars ${BIGDL_HOME}/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar,${BIGDL_HOME}/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar,${BIGDL_HOME}/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar \
+    --jars ${BIGDL_HOME}/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar,${BIGDL_HOME}/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar \
     ncf_train.py --cluster_mode spark-submit --backend spark --model_dir hdfs://172.16.0.105:8020/yushan/
