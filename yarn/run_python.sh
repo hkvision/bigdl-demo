d3f31529380bdd49f570356d1f1ee2106b580690
Kai Huang <huangkaivision@gmail.com>
2022-08-01 19:21:50 +0800

    Update run_python.sh

--- yarn/run_python.sh
+++ yarn/run_python.sh
@@ -1,1 +1,1 @@
-python ncf_train.py --cluster_mode yarn --num_executor 2 --executor_cores 4 --executor_memory 4g --backend tf2 --model_dir hdfs://172.16.0.105:8020/yushan/
+python ncf_train.py --cluster_mode yarn --num_executors 2 --executor_cores 4 --executor_memory 4g --backend tf2 --model_dir hdfs://172.16.0.105:8020/yushan/
